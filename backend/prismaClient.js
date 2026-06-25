0ea9997f02fa592332bb4350f8a3e2fa2c68093a
Oliver Montes <o.e.montes30@gmail.com>
2026-06-25 15:20:42 -0600

    Fix Prisma v7 config and update to Postgres adapter

--- backend/prismaClient.js
+++ backend/prismaClient.js
@@ -1,11 +1,9 @@
 const { PrismaClient } = require('@prisma/client');
-const { PrismaLibSql } = require('@prisma/adapter-libsql');
-const path = require('path');
+const { Pool } = require('pg');
+const { PrismaPg } = require('@prisma/adapter-pg');
 
-const dbPath = path.join(__dirname, 'dev.db');
-const adapter = new PrismaLibSql({
-  url: `file:${dbPath}`,
-});
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
